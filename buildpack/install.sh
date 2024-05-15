f7695355f95d6e80a23a4e4dba79d8ad1908c7d2
Saravanan Vijayappan <saravvij@gmail.com>
2024-05-15 13:01:51 -0700

    Update install.sh

--- buildpack/install.sh
+++ buildpack/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This script will run `npm install --force`
-echo "Running npm install --force"
-npm install --force
+echo "Running npm install --legacy-peer-deps"
+npm install --legacy-peer-deps
 
